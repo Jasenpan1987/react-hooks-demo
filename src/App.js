7e09b687397cbf4b8ec64af95f5841b74d0c2726
Jasenpan1987 <jasenpan@gmail.com>
2018-11-03 18:09:35 +1100

    start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export default () => {
+  const [count, setCount] = useState(0);
 
-export default App;
+  return (
+    <div className="app">
+      <div>Count: {count}</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Add One
+      </button>
+    </div>
+  );
+};
